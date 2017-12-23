02ed9bb035e4d1f6bae187eb66494c4e4062fef9
Shalagin Andrey <andreyshalagin@gmail.com>
2017-12-23 11:20:57 +0300

    Задание 2.3. Добавлены строковые координаты в результат вывода

--- sandbox/src/main/java/hometask1/CoordinateProgram.java
+++ sandbox/src/main/java/hometask1/CoordinateProgram.java
@@ -1,16 +1,14 @@
 package hometask1;
 
+import static hometask1.Point.distance;
+
 public class CoordinateProgram {
     public static void main(String[] args) {
+        Point p1 = new Point(2,5);
+        Point p2 = new Point (7,5);
 
-        double distanceBtwPoints = distance(new Point(2,2), new Point (7,2));
-        System.out.println("Расстояние между точками равно " + distanceBtwPoints);
+        System.out.println("Расстояние между точкой c координатами " + p1.getCoordinate() + " " +
+                "и точкой с координатами " + p2.getCoordinate() + " равно " + distance(p1,p2));
     }
 
-    // вычисляем расстояние между двумя точками
-    public static double distance(Point p1, Point p2){
-        double a = Math.pow((p2.x - p1.x), 2);
-        double b = Math.pow((p2.y - p1.y), 2);
-        return Math.sqrt(a + b);
-    }
 }
--- sandbox/src/main/java/hometask1/Point.java
+++ sandbox/src/main/java/hometask1/Point.java
@@ -1,14 +1,25 @@
 package hometask1;
 
 public class Point {
-
+    // атрибуты
     public double x;
     public double y;
 
+    // конструктор
     public Point(double x, double y){
         this.x = x;
         this.y = y;
     }
+    
+    // вычисляем расстояние между двумя точками
+    public static double distance(Point p1, Point p2){
+        double a = Math.pow((p2.x - p1.x), 2);
+        double b = Math.pow((p2.y - p1.y), 2);
+        return Math.sqrt(a + b);
+    }
 
-
+    // метод для получения координат точки
+    public String getCoordinate(){
+        return "(" + this.x + "; " + this.y + ")";
+    }
 }
