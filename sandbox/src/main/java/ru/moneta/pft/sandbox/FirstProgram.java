d7194bc44affa48dd48a8b2101485b30d960f4df
Shalagin Andrey <andreyshalagin@gmail.com>
2017-12-23 09:45:10 +0300

    Лекция 1.7. Функции

--- sandbox/src/main/java/ru/moneta/pft/sandbox/FirstProgram.java
+++ sandbox/src/main/java/ru/moneta/pft/sandbox/FirstProgram.java
@@ -7,5 +7,20 @@ public class FirstProgram {
         double l = 5;
         double s = l * l;
         System.out.println("Площать квадрата со стороной " + l + " = " + s);
+
+        double length = 7;
+        System.out.println("Площадь квадрата со стороной " + length + " равна " + area(length));
+
+        double a = 5;
+        double b = 10;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + area(a, b));
+    }
+
+    public static double area (double l){
+        return l * l;
+    }
+
+    public static double area (double a, double b){
+        return a * b;
     }
 }
