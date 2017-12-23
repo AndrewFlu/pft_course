59c477c5b230669fa3544e73ed0ad21ec92a6822
Shalagin Andrey <andreyshalagin@gmail.com>
2017-12-23 11:41:11 +0300

    Решено. Задание 2.
    Расчет расстояния между точками реализован через вызов статического метода (функции).

--- sandbox/src/main/java/hometask1/CoordinateProgram.java
+++ sandbox/src/main/java/hometask1/CoordinateProgram.java
@@ -3,6 +3,7 @@ package hometask1;
 import static hometask1.Point.distance;
 
 public class CoordinateProgram {
+
     public static void main(String[] args) {
         Point p1 = new Point(2,5);
         Point p2 = new Point (7,5);
