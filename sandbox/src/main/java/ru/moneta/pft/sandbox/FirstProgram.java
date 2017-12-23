3b09f978fdc4a3b7b9d820ff429a32774e653e55
Shalagin Andrey <andreyshalagin@gmail.com>
2017-12-23 10:17:28 +0300

    Лекция 1.9

--- sandbox/src/main/java/ru/moneta/pft/sandbox/FirstProgram.java
+++ sandbox/src/main/java/ru/moneta/pft/sandbox/FirstProgram.java
@@ -5,19 +5,12 @@ public class FirstProgram {
         String someString = "Hello! ";
         System.out.println(someString + "This is simple java program");
 
-        Square s = new Square(7);
-        System.out.println("Площадь квадрата со стороной " + s.l + " равна " + area(s));
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " равна " + s.area());
 
-        Rectangle r = new Rectangle(3,7);
+        Rectangle r = new Rectangle(5,7);
 
-        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + area(r));
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + r.area());
     }
 
-    public static double area (Square s){
-        return s.l * s.l;
-    }
-
-    public static double area (Rectangle r){
-        return r.a * r.b;
-    }
 }
