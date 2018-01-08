96e8476e4c8552021209ab0b492fa4a6a3a18d0e
Shalagin Andrey <andreyshalagin@gmail.com>
2018-01-08 22:11:27 +0300

    Решено. Задание 7
    - Добавлена конфигурация запуска трех браузеров (IE, Firefox, Chrome)
    - Исполняемые файлы (драйверы браузеров Chrome и IE) помещены в директорию проекта.

--- addressbook-web-tests/src/test/java/ru/moneta/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/moneta/pft/addressbook/tests/TestBase.java
@@ -8,7 +8,7 @@ import ru.moneta.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.GOOGLECHROME);
 
     @BeforeMethod
     public void setUp() throws Exception {
