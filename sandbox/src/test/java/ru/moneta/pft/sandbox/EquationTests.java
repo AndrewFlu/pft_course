d75fae9bde9f3e1dd0f3022dc80b63e0377fd8fd
Shalagin Andrey <andreyshalagin@gmail.com>
2018-01-08 16:23:58 +0300

    Лекция 3.1
    Добавлены 2 класса в модуль sandbox:
    - основной: Equation
    - тестовый: EquationTests

---
+++ sandbox/src/test/java/ru/moneta/pft/sandbox/EquationTests.java
@@ -0,0 +1,24 @@
+package ru.moneta.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class EquationTests {
+    @Test
+    public void test0(){
+        Equation e = new Equation(1,1,1);
+        Assert.assertEquals(e.getNumber(), 0);
+    }
+
+    @Test
+    public void test1(){
+        Equation e = new Equation(1,2,1);
+        Assert.assertEquals(e.getNumber(), 1);
+    }
+
+    @Test
+    public void test2(){
+        Equation e = new Equation(2,5,2);
+        Assert.assertEquals(e.getNumber(), 2);
+    }
+}
