670b6d0cf01e7542a03c1c197672611de3e92c44
Shalagin Andrey <andreyshalagin@gmail.com>
2018-01-25 12:31:35 +0300

    Лекция 5.2
    Структуризация кода тестов для групп (модификация и удаление):
    - переход на страницу групп и проверка наличия хотя бы одной группы вынесена в отдельный метод и помечена аанотацией @BeforeMethod

--- addressbook-web-tests/src/test/java/ru/moneta/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/moneta/pft/addressbook/tests/GroupDeletionTests.java
@@ -1,19 +1,23 @@
 package ru.moneta.pft.addressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.moneta.pft.addressbook.model.GroupData;
 
 import java.util.List;
 
 public class GroupDeletionTests extends TestBase{
-    
-    @Test
-    public void testGroupDeletion() {
+
+    @BeforeMethod
+    public void ensurePreconditions(){
         app.getNavigationHelper().gotoGroupPage();
         if(! app.getGroupHelper().isThereAnGroup()){
             app.getGroupHelper().createGroup(new GroupData("group2", "header 2", null));
         }
+    }
+    @Test
+    public void testGroupDeletion() {
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().deleteSelectedGroups();
--- addressbook-web-tests/src/test/java/ru/moneta/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/moneta/pft/addressbook/tests/GroupModificationTests.java
@@ -1,6 +1,7 @@
 package ru.moneta.pft.addressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.moneta.pft.addressbook.model.GroupData;
 
@@ -8,23 +9,28 @@ import java.util.Comparator;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase{
-    @Test
-    public void testGroupModification(){
+
+    @BeforeMethod
+    public void ensurePreconditions() {
         app.getNavigationHelper().gotoGroupPage();
         if(! app.getGroupHelper().isThereAnGroup()){
             app.getGroupHelper().createGroup(new GroupData("NewGroup", "ModHeader 3", "ModFooter 3"));
         }
+    }
+    @Test
+    public void testGroupModification(){
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        app.getGroupHelper().selectGroup(before.size() - 1);
+        int index = before.size() - 1;
+        GroupData groupData = new GroupData(before.get(index).getId(),"group1", "header 2", "footer 3");
+        app.getGroupHelper().selectGroup(index);
         app.getGroupHelper().initGroupModification();
-        GroupData groupData = new GroupData(before.get(before.size() - 1).getId(),"group1", "header 2", "footer 3");
         app.getGroupHelper().fillGroupForm(groupData);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
 
-        before.remove(before.size() - 1);
+        before.remove(index);
         before.add(groupData);
         Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
         before.sort(byId);
