3a07ee68a7e5fed2dfd1bbc54840843d2de3ba6a
Shalagin Andrey <andreyshalagin@gmail.com>
2018-01-28 17:09:39 +0300

    Лекция 5.8 Хеширование
    Добавлено хэширование (предварительные проверки) для тестов по группам
    - добавлены ассерты перед созданием тяжеловесного метода создания множества after

--- addressbook-web-tests/src/test/java/ru/moneta/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/moneta/pft/addressbook/tests/GroupCreationTests.java
@@ -19,4 +19,15 @@ public class GroupCreationTests extends TestBase{
         assertThat(after.size(), equalTo(before.size() + 1));
         assertThat(after, equalTo(before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
     }
+
+    @Test
+    public void tesBadGroupCreation() {
+        app.goTo().groupPage();
+        Groups before = app.group().all();
+        GroupData group = new GroupData().withName("Name ' ").withHeader("Header 1");
+        app.group().create(group);
+        assertThat(app.group().count(), equalTo(before.size()));
+        Groups after = app.group().all();
+        assertThat(after, equalTo(before));
+    }
 }
