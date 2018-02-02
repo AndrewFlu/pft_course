cc5a0bf68021b6a278c40decbfc981b568a95c9f
Shalagin Andrey <andreyshalagin@gmail.com>
2018-02-02 09:26:31 +0300

    Лекция 6.4 Параметризация тестовых методов
    - Добавлен DataProvider для генерации тестовых данных в тесте на создание групп
    - в качестве аргумента тест GroupCreationTest принимает один объект (типа GroupData) формируемый DataProvider'ом

--- addressbook-web-tests/src/test/java/ru/moneta/pft/addressbook/Generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/moneta/pft/addressbook/Generators/GroupDataGenerator.java
@@ -41,7 +41,6 @@ public class GroupDataGenerator {
     }
 
     private void save(List<GroupData> groups, File file) throws IOException {
-        System.out.println(new File(".").getAbsolutePath());
         Writer writer = new FileWriter(file);
         for (GroupData group : groups){
             writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
--- addressbook-web-tests/src/test/java/ru/moneta/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/moneta/pft/addressbook/tests/GroupCreationTests.java
@@ -1,19 +1,32 @@
 package ru.moneta.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.moneta.pft.addressbook.model.GroupData;
 import ru.moneta.pft.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase{
 
-    @Test
-    public void testGroupCreation() {
+    @DataProvider
+    public Iterator<Object[]> validGroups(){
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[] {new GroupData().withName("GroupName 1").withHeader("GroupHeader 1").withFooter("GroupFooter 1")});
+        list.add(new Object[] {new GroupData().withName("GroupName 2").withHeader("GroupHeader 2").withFooter("GroupFooter 2")});
+        list.add(new Object[] {new GroupData().withName("GroupName 3").withHeader("GroupHeader 3").withFooter("GroupFooter 3")});
+        return list.iterator();
+    }
+
+    @Test (dataProvider = "validGroups")
+    public void testGroupCreation(GroupData group) {
         app.goTo().groupPage();
         Groups before = app.group().all();
-        GroupData group = new GroupData().withName("Name 2").withHeader("Header 2");
         app.group().create(group);
         assertThat(app.group().count(), equalTo(before.size() + 1));
         Groups after = app.group().all();
