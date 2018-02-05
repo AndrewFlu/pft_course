c9c8b22eb7d443375727f0ada700038ee1df3ee3
Shalagin Andrey <andreyshalagin@gmail.com>
2018-02-05 22:32:38 +0300

    Лекция 6.11 Протоколирование
    - Подключена библиотека для протоколирования Logback-classic
    - создан конфигурационный файл для библиотеки logback
    - использование протоколирования реализовано через базовый класс путем методов с аннотациями @BeforeMethod и @AfterMethod

--- addressbook-web-tests/src/test/java/ru/moneta/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/moneta/pft/addressbook/tests/TestBase.java
@@ -1,11 +1,19 @@
 package ru.moneta.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.moneta.pft.addressbook.appmanager.ApplicationManager;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class TestBase {
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     protected static final ApplicationManager app
             = new ApplicationManager(System.getProperty("browser", BrowserType.GOOGLECHROME));
@@ -15,13 +23,18 @@ public class TestBase {
         app.init();
     }
 
-    @AfterSuite
+    @AfterSuite (alwaysRun = true)
     public void tearDown() {
         app.stop();
     }
 
+    @BeforeMethod
+    public void logTestStart(Method m, Object [] p){
+        logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+    }
 
-    public ApplicationManager getApp() {
-        return app;
+    @AfterMethod (alwaysRun = true)
+    public void logTestStop(Method m, Object[] p){
+        logger.info("Stop test " + m.getName() + " with parameters " + Arrays.asList(p));
     }
 }
