2dddf2c5eef8c967957ed3bcb87c75ea5d6c4ad1
Shalagin Andrey <andreyshalagin@gmail.com>
2018-02-05 16:08:38 +0300

    Решено. Задание 14.
    - Описание тестового стенда вынесено в файл local.properties
    - Добавлены настройки в файл конфигурации сборщика для разделения тестов на группы: тесты для контактов и тесты для групп, а также на указание файла с описанием тестового стенда

--- addressbook-web-tests/src/test/java/ru/moneta/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/moneta/pft/addressbook/appmanager/ApplicationManager.java
@@ -7,10 +7,15 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
 
+    private final Properties properties;
     WebDriver wd;
     private ContactHelper contactHelper;
     private SessionHelper sessionHelper;
@@ -18,25 +23,32 @@ public class ApplicationManager {
     private GroupHelper groupHelper;
     private String browser;
 
-    public ApplicationManager(String browser) {
+    public ApplicationManager(String browser){
         this.browser = browser;
+        properties = new Properties();
     }
 
-    public void init() {
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
         if (browser.equals(BrowserType.FIREFOX)){
             wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true).setBinary("C://Program Files/Mozilla Firefox 53.2 ESR/firefox.exe"));
+            wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
         } else if(browser.equals(BrowserType.GOOGLECHROME)){
             wd = new ChromeDriver();
+            wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
         } else if (browser.equals(BrowserType.IE)){
             wd = new InternetExplorerDriver();
+            wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         }
-        wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        wd.get("http://localhost/addressbook/");
+
+        wd.get(properties.getProperty("web.baseUrl"));
         contactHelper = new ContactHelper(wd);
         sessionHelper = new SessionHelper(wd);
         navigationHelper = new NavigationHelper(wd);
         groupHelper = new GroupHelper(wd);
-        sessionHelper.login("admin", "secret");
+        sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
     }
 
     public void stop() {
