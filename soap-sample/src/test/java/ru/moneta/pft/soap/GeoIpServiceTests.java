93f623a27ec038309ae7f2043a67bad7e1607d4e
Shalagin Andrey <andreyshalagin@gmail.com>
2018-02-24 07:21:35 +0300

    Лекция 9.2 Взаимодействие по SOAP протоколу
    - Подключена зависимость плагина wsdl2java для преобразования оптсания API  в формате WSDL в код Java
    - Сгенерирован java код по файлу API с описанием сервисов в формате WSDL.
    - Добавлены тесты на проверку IP адресов.

--- soap-sample/src/test/java/ru/moneta/pft/soap/GeoIpServiceTests.java
+++ soap-sample/src/test/java/ru/moneta/pft/soap/GeoIpServiceTests.java
@@ -1,11 +1,23 @@
 package ru.moneta.pft.soap;
 
+import net.webservicex.GeoIP;
+import net.webservicex.GeoIPService;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
 
 public class GeoIpServiceTests {
 
     @Test
     public void testMyIp(){
+        GeoIP geoIP = new GeoIPService().getGeoIPServiceSoap12().getGeoIP("77.40.29.226");
+        assertEquals(geoIP.getCountryCode(), "RUS");
+    }
 
+    @Test
+    public void testInvalidIp(){
+        GeoIP geoIP = new GeoIPService().getGeoIPServiceSoap12().getGeoIP("77.40.29.xxx");
+        assertEquals(geoIP.getReturnCodeDetails(), "Invalid IP address");
     }
 }
