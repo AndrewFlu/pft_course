efec0c2f14cca169d20e9aa43d80d82a511a100f
Shalagin Andrey <andreyshalagin@gmail.com>
2018-03-02 22:38:04 +0300

    Тест исправлен

--- sandbox/src/test/java/ru/moneta/pft/sandbox/SquareTests.java
+++ sandbox/src/test/java/ru/moneta/pft/sandbox/SquareTests.java
@@ -7,6 +7,6 @@ public class SquareTests {
     @Test
     public void testArea(){
         Square square = new Square(5);
-        Assert.assertEquals(square.area(), 21.0);
+        Assert.assertEquals(square.area(), 25.0);
     }
 }
