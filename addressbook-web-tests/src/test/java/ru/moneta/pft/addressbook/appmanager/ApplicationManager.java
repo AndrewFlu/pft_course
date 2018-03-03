4016129b824bb364142fc30aad4338c9813bf105
Shalagin Andrey <andreyshalagin@gmail.com>
2018-03-03 08:27:04 +0300

    Лекция 10.2 Удаленный запуск браузера Selenium Server
    Реализован удаленный запуск браузера на удаленном сервере

--- addressbook-web-tests/src/test/java/ru/moneta/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/moneta/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,15 +1,19 @@
 package ru.moneta.pft.addressbook.appmanager;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -34,16 +38,21 @@ public class ApplicationManager {
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
         dbHelper = new DbHelper();
-
-        if (browser.equals(BrowserType.FIREFOX)){
-            wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true).setBinary(properties.getProperty("browser.firefox")));
-            wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        } else if(browser.equals(BrowserType.GOOGLECHROME)){
-            wd = new ChromeDriver();
-            wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        } else if (browser.equals(BrowserType.IE)){
-            wd = new InternetExplorerDriver();
-            wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        if ("".equals(properties.getProperty("selenium.server"))) {
+            if (browser.equals(BrowserType.FIREFOX)) {
+                wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true).setBinary(properties.getProperty("browser.firefox")));
+                wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+            } else if (browser.equals(BrowserType.GOOGLECHROME)) {
+                wd = new ChromeDriver();
+                wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+            } else if (browser.equals(BrowserType.IE)) {
+                wd = new InternetExplorerDriver();
+                wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+            }
+        } else {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName(browser);
+            wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities)
         }
 
         wd.get(properties.getProperty("web.baseUrl"));
